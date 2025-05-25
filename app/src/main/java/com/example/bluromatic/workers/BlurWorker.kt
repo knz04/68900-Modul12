402a32662577c07637369e741209f1c4b7621a1c
hawyeet <53439203+knz04@users.noreply.github.com>
2025-05-25 08:59:14 +0700

    10. Input data and output data

--- app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -2,10 +2,14 @@ package com.example.bluromatic.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -21,21 +25,32 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, 
         )
 
         return withContext(Dispatchers.IO) {
+
+            val resourceUri = inputData.getString(KEY_IMAGE_URI)
+            val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
+
             delay(DELAY_TIME_MILLIS)
 
             return@withContext try {
-                val picture = BitmapFactory.decodeResource(
-                    applicationContext.resources,
-                    R.drawable.android_cupcake
+                require(!resourceUri.isNullOrBlank()) {
+                    val errorMessage =
+                        applicationContext.resources.getString(R.string.invalid_input_uri)
+                        Log.e(TAG, errorMessage)
+                        errorMessage
+                }
+                val resolver = applicationContext.contentResolver
+
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
                 )
 
-                val output = blurBitmap(picture, 1)
+                val output = blurBitmap(picture, blurLevel)
 
                 val outputUri = writeBitmapToFile(applicationContext, output)
 
-                makeStatusNotification("Output is $outputUri", applicationContext)
+                val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
 
-                Result.success()
+                Result.success(outputData)
             } catch (throwable: Throwable) {
                 Log.e(
                     TAG,
